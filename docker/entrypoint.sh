406229a73fd07dae1a36c5ddd2b145cbc6300eba
sjkoelle <s_uberduck_ai@uberduck-sam-t4-west2c.us-west2-c.c.uberduck.internal>
2021-12-14 01:45:33 +0000

    edited entrypoitn and dockerfile untested

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-#sleep 3600
-git clone -b sam-exp $4
-#git clone -b sam-exp https://github.com/uberduck-ai/uberduck-ml-exp.git
-#sleep 3600
-#echo $1
-#echo $2
-#echo $3
-echo $4
+UBMLEXP=$4
+git clone -b sam-exp $UBMLEXP
 gcloud auth activate-service-account --key-file=/secrets/gcloud_key.json
 export GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcloud_key.json
-gspath_data=$1
-gspath_model=$2
-gspath_results=$3
-#sleep 3600
-gsutil cp -r $gspath_data /data
-gsutil cp $gspath_model /root/models
-#gcsfuse uberduck-vertex-v0 /ub_bucket/
-gcsfuse uberduck-vertex-v0/results /root/results
-#gsutil cp -r $gspath_results /root/results
-#sleep 3600
-echo $4
-#gcloud auth application-default login --quiet
-echo $4
-sleep 3600
-python -m uberduck_ml_dev.exec.train_tacotron2 --config ./uberduck-ml-exp/experiments/taco2_lj_lachow/config.json
+DATA_DIR=$1
+MODEL_DIR=$2
+RESULT_DIR=$3
+WARM_START_NAME=$5
+FILELIST_TRAIN=$6
+FILELIST_VAL=$7
+NAME=$8
+LOG_DIR=${RESULT_DIR}/${NAME}/logs
+CHECKPOINT_PATH=${RESULT_DIR}/${NAME}/checkpoints
+gcsfuse $RESULT_DIR /results #RESULT_DIR = uberduck-ver-v0/results
+gcsfuse $DATA_DIR /data #DATA_DIR = uberduck-vertex-v0/data
+gcsfuse $MODEL_DIR /models #MODEL_DIR = uberduck-vertex-v0/models
+python -m uberduck_ml_dev.exec.train_tacotron2 --config ./uberduck-ml-exp/experiments/taco2_lj_lachow/config.json --log_dir $LOG_DIR --checkpoint_path $CHECKPOINT_PATH --training_audiopaths_and_text $FILELIST_TRAIN --val_audiopaths_and_text $FILELIST_VAL --warm_start_name $WARM_START_NAME
