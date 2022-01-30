8d46e187495bf00ce07900d869ac8faa90230691
sjkoelle <s_uberduck_ai@uberduck-sam-t4-west2c.us-west2-c.c.uberduck.internal>
2022-01-30 21:12:38 +0000

    monitoring batch update

--- uberduck_ml_dev/monitoring/generate.py
+++ uberduck_ml_dev/monitoring/generate.py
@@ -4,6 +4,7 @@ __all__ = ['MODEL_TYPES']
 
 # Cell
 from ..data_loader import prepare_input_sequence
+from ..data.batch import Batch
 
 MODEL_TYPES = ["OD", "OD", "OD", "OD", "OD", "D"]
 
@@ -13,7 +14,11 @@ def _get_inference(model, vocoder, texts, speaker_ids, symbol_set, arpabet):
     text_padded, input_lengths = prepare_input_sequence(
         texts, cpu_run=False, arpabet=arpabet, symbol_set=symbol_set
     )
-    input_ = text_padded, input_lengths, speaker_ids
+    input_ = Batch(
+        text_int_padded=text_padded,
+        input_lengths=input_lengths,
+        speaker_ids=speaker_ids,
+    )
     output = model.inference(input_)
     audio = vocoder.infer(output[1][:1])
     return audio
